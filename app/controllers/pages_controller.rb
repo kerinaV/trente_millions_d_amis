0837db4e031ee1a48ba3c8fb7abecf5b370a5398
kerina voltigeur <kerina.voltigeur@gmail.com>
2022-03-21 16:07:42 -0400

    site version un

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
+    @hostels = Hostel.first(2)
+    if params[:reservation]
+      redirect_to root_path, notice: 'Votre réservation a bien été enregistrée'
+    end
   end
 end
